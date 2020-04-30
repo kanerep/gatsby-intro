587571d25663709752911514847f10985422581b
Kane Elliott-Peace <kane.e@mmtdigital.co.uk>
2020-04-30 10:51:51 +0100

    Created the Header component, created a styled link component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Header from '../components/Header';
 
 const Layout = ({ children }) => (
   <>
@@ -53,7 +54,7 @@ const Layout = ({ children }) => (
         }
       `}
     />
-    <header></header>
+    <Header />
     <main
       css={css`
         margin: 2rem auto 4rem;
