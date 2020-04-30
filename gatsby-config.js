2af3a8a644b9270fb072d68e005297e05d474448
Kane Elliott-Peace <kane.e@mmtdigital.co.uk>
2020-04-30 13:29:15 +0100

    Create a graphql hook so we can query for the metadata in the gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description:
+      'A workshop by Jason Lengstorf to teach the basics of Gatsby 🚀',
+  },
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayout: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
