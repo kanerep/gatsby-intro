37ebc9da1cade3affb831cbf1101e8ee43fd0d57
Kane Elliott-Peace <kane.e@mmtdigital.co.uk>
2020-05-01 11:47:26 +0100

    MDX blog posts created,  template post created,  using Node and GraphQL to dynamically create pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>
-      Welcome to my personal blog!
-      <span role="img" aria-label="emoji-waving-hello">
-        👋
-      </span>
-    </p>
-    <Link to="/about/">About me &rarr;</Link>
-  </Layout>
-);
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/PostPreview';
+
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>
+        Welcome to my personal blog!
+        <span role="img" aria-label="emoji-waving-hello">
+          👋
+        </span>
+      </p>
+      <Link to="/about/">About me &rarr;</Link>
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
